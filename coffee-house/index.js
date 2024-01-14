d6fb6a07c202aa2616a1aae274a2bcffb1b8db71
elenaf007 <96072892+elenaf007@users.noreply.github.com>
2024-01-14 15:02:34 -0600

    feat:slider fo favorite coffee

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -4,12 +4,43 @@ const burgerMenuCloseBtn=document.getElementById('btn-menu-active');
 const menu=document.querySelector('.burger-btn');
 const burgerBar1=document.querySelector('.bar-1');
 const burgerBar2=document.querySelector('.bar-2');
+//menu
 menu.addEventListener('click', ()=>{
     menu.classList.toggle('open');
     burgerBar1.classList.toggle('open');
     burgerBar2.classList.toggle('open');
     burgerMenuBlock.classList.toggle('navbar')
 })
+
+//slider for Favorite coffee
+const slider=document.querySelector('.slider');
+const leftArrow=document.querySelector('.btn-left');
+const rightArrow=document.querySelector('.btn-right');
+var sectionIndex=0;
+leftArrow.addEventListener('click', function(){
+  sectionIndex=(sectionIndex>0)? sectionIndex-1 : 2;
+  //sectionIndex+=1;
+  slider.style.transform='translate('+ (sectionIndex) * -33 +'%)';
+})
+rightArrow.addEventListener('click', function(){
+  sectionIndex=(sectionIndex<2)? sectionIndex+1 : 0;
+  //sectionIndex+=1;
+  slider.style.transform='translate('+ (sectionIndex) * -33 +'%)';
+})
+
+//selectors
+document.querySelectorAll('.slider-nav li').forEach(function(indicator, index){
+  indicator.addEventListener('click', function(){
+    sectionIndex=index;
+    console.log('selected', document.querySelectorAll('.slider-nav  li'));
+    
+    document.querySelector('.slider-nav  li.selected').classList.remove('selected');
+    indicator.classList.add('selected');
+    slider.style.transform='translate('+ (sectionIndex) * -33 +'%)';
+    
+  })
+})
+
 
 // opening burger menu function
 function openBurgerMenu(){
